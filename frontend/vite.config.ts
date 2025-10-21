e61c1389e0841dc37f36a6b6070333737010abca
garrik <garrik@mail.ru>
2025-10-21 15:14:32 +0300

    fix env for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const proxyPort: string = process.env.PORT || "8080";
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
@@ -8,7 +10,7 @@ export default defineConfig({
     ],
     server: {
         proxy: {
-            '/api': 'http://localhost:8080',
+            '/api': 'http://localhost:'+proxyPort,
         }
     }
 })
